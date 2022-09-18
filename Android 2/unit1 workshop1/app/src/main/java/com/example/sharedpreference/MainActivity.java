9906971061bbe35f2e76b64b03ebf18ddc592743
Dineshkumar200 <75235789+Dineshkumar200@users.noreply.github.com>
2022-09-18 21:31:37 +0530

    Add files via upload

---
+++ Android 2/unit1 workshop1/app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText name, age;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = findViewById(R.id.editTextTextPersonName);
+        age = findViewById(R.id.editTextTextPersonName2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+
+        String s1 = sh.getString("name", "");
+        int a = sh.getInt("age", 0);
+
+        name.setText(s1);
+        age.setText(String.valueOf(a));
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        // write all the data entered by the user in SharedPreference and apply
+        myEdit.putString("name", name.getText().toString());
+        myEdit.putInt("age", Integer.parseInt(age.getText().toString()));
+        myEdit.apply();
+    }
+}
+
+
+
